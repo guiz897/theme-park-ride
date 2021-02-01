3ddb766b0d196084c6ae11e0d81e5dde7285080d
guiz897 <78160388+guiz897@users.noreply.github.com>
2021-02-01 16:36:45 +0100

    Update ThemeParkRideApplication.java

--- src/main/java/com/exemple/ThemeParkRideApplication.java
+++ src/main/java/com/exemple/ThemeParkRideApplication.java
@@ -18,6 +18,7 @@ public class ThemeParkRideApplication {
 	@Bean
 	public CommandLineRunner sampleData(ThemeParkRideRepository repository) {
 		return (args) -> {
+			blablablablabla
 			repository.save(new ThemeParkRide("Rollercoaster", "Train ride that speeds you along.", 5, 3));
 			repository.save(new ThemeParkRide("Log flume", "Boat ride with plenty of splashes.", 3, 2));
 			repository.save(new ThemeParkRide("Teacups", "Spinning ride in a giant tea-cup.", 2, 4));
